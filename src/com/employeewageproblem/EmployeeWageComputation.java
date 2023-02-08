55f862fe1e50c58a06195710f8b6f6f7ac25d43c
dbasumatary <basudhondip@gmail.com>
2023-02-08 20:12:09 +0530

    Added: Part time employee and their wage

--- src/com/employeewageproblem/EmployeeWageComputation.java
+++ src/com/employeewageproblem/EmployeeWageComputation.java
@@ -1,17 +1,23 @@
 package com.employeewageproblem;
+/*Added part-time employee and wage to previous program*/
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         // Constants
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         //Variables
         int employeeHours;
         int employeeWage;
         // Computation
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is present");
+            System.out.println("Employee is present full time");
             employeeHours = 8;
+        }
+        else if (empCheck == IS_PART_TIME) {
+            System.out.println("Employee is present part time");
+            employeeHours = 4;
         }
         else {
             System.out.println("Employee is absent");
